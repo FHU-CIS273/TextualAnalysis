3c1134524f6d21c6ff54f82ae8d89d6e3cce3fe7
Kenan Casey <kenancasey@gmail.com>
2019-10-18 10:12:22 -0500

    initial commit

---
+++ TextualAnalysis/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TextualAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ComputeWordFrequenciesFromFile("../../../Data/small.txt");
+        }
+
+
+        public static Dictionary<string, int> ComputeWordFrequencies(string s, bool ignoreStopWords = false)
+        {
+            var wordCounts = new Dictionary<string, int>();
+
+            // do all the work
+
+
+            return wordCounts;
+        }
+
+        public static Dictionary<string, int> ComputeWordFrequenciesFromFile(string path, bool ignoreStopWords = false)
+        {
+            // open the file
+
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string text = streamReader.ReadToEnd();
+
+                Console.WriteLine(text);
+
+                var words = text.Split(" \n.");
+
+                foreach( string s in words)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            // read file contents into a string
+
+
+            var wordCounts = new Dictionary<string, int>();
+
+            // do all the work
+
+
+            return wordCounts;
+        }
+
+
+    }
+}
